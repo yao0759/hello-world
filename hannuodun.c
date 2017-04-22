0e3073500a0cf6db619f89064b327238d643efd8
Yao Hua <yaohuayaozhihua@outlook.com>
2017-04-23 00:19:22 +0800

    Update hannuodun.c

--- hannuodun.c
+++ hannuodun.c
@@ -1,4 +1,8 @@
 #汉诺顿图形输出（从51CTO中的课程）
 #include<stdio.h>
 #include<stdlib.h>
-void show<int a[10][3]>
+
+int a[10][3]={0};//数据初始化
+void show（int a[10][3]）；
+void hannuo(int n,char A,char B,char C);
+void move()
