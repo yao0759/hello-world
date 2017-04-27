218d1b417b966cbe52db5b516bc1a16f61db0cf7
Yao Hua <yaohuayaozhihua@outlook.com>
2017-04-27 23:14:32 +0800

    Update max.c

--- max.c
+++ max.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-int main()
-{
-  int a,b,c;
-  int max(int x,int y);
-  c=max(a,b);
-  printf("The max is %d",c);
-  return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+  //int a,b,c;
+  //int max(int x,int y);
+  //c=max(a,b);
+  //printf("The max is %d",c);
+  //return 0;
+//}
 int max(int x,int y)
 {
   int z;
