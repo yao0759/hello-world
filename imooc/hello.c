e3ce24c6a652f6d27d83427e18ac1db34dccb919
Yao Hua <yaohuayaozhihua@outlook.com>
2017-04-28 18:12:37 +0800

    Update hello.c

--- imooc/hello.c
+++ imooc/hello.c
@@ -5,8 +5,10 @@ int main()
  {
     int a1=21;
     int a2=33;
-    int maxNum=max(a1,a2);
-    int minNum=min(a1,a2);
+    maxNum=max(a1,a2);
+    minNum=min(a1,a2);
+    int maxNum(int x,int y);
+    int minNum(int x,int y);
     printf("the max number is %d\n",maxNum);
     printf("the min number is %d\n",minNum);
     return 0;
